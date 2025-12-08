10fdbcc7d376acf7cd6ba8f1a81571da39c8b20c
narvdeshwar <sam.07soni@gmail.com>
2025-12-08 12:25:21 +0530

    Realtime collboration

---
+++ src/lib/data/realtime.js
@@ -0,0 +1,22 @@
+export const features = [
+  {
+    title: "Activate the Power of Your Data and Analytics.",
+    description:
+      "Weave your data and analytics directly into the daily decision-making happening across your core business and operational teams. Capture decisions for continuous learning.",
+  },
+  {
+    title: "Collaboration, Supercharged.",
+    description:
+      "Achieve real-time collaboration between your data, analytics, and operational teams. Integrate decision-making in a common logic layer, driving meaningful action as conditions evolve.",
+  },
+  {
+    title: "The Best Way to Build.",
+    description:
+      "Compose AI-powered workflows on a scalable architecture that re-uses your ontology's multi-modal objects, actions, and processes.",
+  },
+  {
+    title: "No Duplication.",
+    description:
+      "Incorporate data and models from your enterprise architecture — without duplicating the underlying assets or fracturing existing sources of truth.",
+  },
+];
--- src/views/platforms/foundary/Foundary.jsx
+++ src/views/platforms/foundary/Foundary.jsx
@@ -1,11 +1,13 @@
 import PalantirFoundry from "./comp/HeroSection";
 import Ontalogy from "./comp/Ontalogy";
+import OntologySection from "./comp/RealTimeColl";
 
 function Foundary() {
   return (
     <>
       <PalantirFoundry />
       <Ontalogy/>
+      <OntologySection/>
     </>
   );
 }
